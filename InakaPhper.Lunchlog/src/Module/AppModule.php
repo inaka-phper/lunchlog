5c3b31870d221215c9d9465bfb5687f88ab03e67
phper <inaka.phper@gmail.com>
2016-03-01 22:39:21 +0900

    [delete] remove phpdotenv code.

--- InakaPhper.Lunchlog/src/Module/AppModule.php
+++ InakaPhper.Lunchlog/src/Module/AppModule.php
@@ -2,9 +2,7 @@
 
 namespace InakaPhper\Lunchlog\Module;
 
-use BEAR\AppMeta\AppMeta;
 use BEAR\Package\PackageModule;
-use Dotenv\Dotenv;
 use InakaPhper\Lunchlog\Entity\Shop;
 use Ray\AuraSqlModule\AuraSqlModule;
 use Ray\Di\AbstractModule;
@@ -16,13 +14,6 @@ class AppModule extends AbstractModule
      */
     protected function configure()
     {
-        // app meta
-        $appMeta = new AppMeta('InakaPhper\Lunchlog');
-        // dot env
-        $dotenv = new Dotenv($appMeta->appDir);
-        $dotenv->load();
-        $dotenv->required(['PDO_DSN', 'PDO_USER', 'PDO_PASSWORD']);
-
         $this->install(new PackageModule);
         $this->install(new AuraSqlModule(getenv('PDO_DSN'), getenv('PDO_USER'), getenv('PDO_PASSWORD')));
 
